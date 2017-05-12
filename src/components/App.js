1f9895fe70f75a91b6a5695e1fb572937954d905
Chinedu Daniel <chinedudaniel7@gmail.com>
2017-05-12 14:13:31 +0100

    Initial design of homepage components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,7 @@
 import React, { Component } from 'react';
-<<<<<<< bc164177efdb96348ac9f912e3099f5721512922
 import Header from './common/Header';
 import Footer from './common/Footer';
 import HomePage from './home/HomePage';
-=======
-import HomePage from "./home/HomePage";
->>>>>>> Integrate sass, bootstrap and react bootstrap
 import './App.css';
 
 class App extends Component {
