78ad237d67ecf16c21d9e421d48af62835506340
Tijesunimi Peters <tijesunimi.peters@andela.com>
2017-05-13 21:31:37 +0100

    Login Modal completed

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,48 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
+import Login from '../login/Login';
 import './Header.css';
 
-const Header = (props) => {
-  return (
-    <header className="main-header">
-      <nav className="navigation">
-        <div className="top-nav">
-          <Grid>
-            <Row>
-              <Col md={6} mdOffset={2} className="search-container">
-                <input type="search" placeholder="Search for stores, brand and more..." className="form-control" />
-                <i className="fa fa-search"></i>
-              </Col>
+class Header extends Component {
+  constructor(props) {
+    super(props);
 
-              <Col md={3} className="text-right authentication-actions">
-                <a href="#" className="login-btn">
-                  Login
-                </a>
+    this.state = {
+      show: false,
+    }
 
-                <a href="#" className="signup-btn btn btn-rounded btn-default">
-                  Signup
-                </a>
-              </Col>
-            </Row>
-          </Grid>
-        </div>
+    this.showModal = this.showModal.bind(this);
+    this.close = this.close.bind(this);
+  }
 
-        <div className="bottom-nav">
-          <Grid>
-            <Row>
-              <Col md={6} mdOffset={2} className="navigation-links">
-                <a href="#" className="active">Home</a>
-                <a href="#">Categories</a>
-                <a href="#">Deals</a>
-                <a href="#">Stores</a>
-                <a href="#">Specials</a>
-              </Col>
-            </Row>
-          </Grid>
-        </div>
-      </nav>
-    </header>
-  );
+  showModal() {
+    this.setState({show: true})
+  };
+
+  close() {
+    this.setState({show: false});
+  }
+
+
+  render() {
+    return (
+      <header className="main-header">
+        <Login showModal={this.state.show} close={this.close} />
+        <nav className="navigation">
+          <div className="top-nav">
+            <Grid>
+              <Row>
+                <Col md={6} mdOffset={2} className="search-container">
+                  <input type="search" placeholder="Search for stores, brand and more..." className="form-control" />
+                  <i className="fa fa-search"></i>
+                </Col>
+
+                <Col md={3} className="text-right authentication-actions">
+                  <a href="#" className="login-btn" onClick={this.showModal}>
+                    Login
+                  </a>
+
+                  <a href="#" className="signup-btn btn btn-rounded btn-default">
+                    Signup
+                  </a>
+                </Col>
+              </Row>
+            </Grid>
+          </div>
+
+          <div className="bottom-nav">
+            <Grid>
+              <Row>
+                <Col md={6} mdOffset={2} className="navigation-links">
+                  <a href="#" className="active">Home</a>
+                  <a href="#">Categories</a>
+                  <a href="#">Deals</a>
+                  <a href="#">Stores</a>
+                  <a href="#">Specials</a>
+                </Col>
+              </Row>
+            </Grid>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default Header;
